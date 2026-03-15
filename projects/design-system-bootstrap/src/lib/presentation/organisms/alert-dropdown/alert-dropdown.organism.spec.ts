718abb910ead82eccc198fc225cc51aab1cc0f76
JOR3NTO <jorgeedcobo@gmail.com>
2026-03-15 12:41:15 -0500

    test(organism): agregar pruebas de AlertDropdownOrganism

--- projects/design-system-bootstrap/src/lib/presentation/organisms/alert-dropdown/alert-dropdown.organism.spec.ts
+++ projects/design-system-bootstrap/src/lib/presentation/organisms/alert-dropdown/alert-dropdown.organism.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { MOCK_ALERT_DROPDOWN_CONFIG } from '../../../mocks/alert-dropdown.mocks';
 import { AlertDropdownOrganism } from './alert-dropdown.organism';
 
 describe('AlertDropdownOrganism', () => {
@@ -17,5 +19,29 @@ describe('AlertDropdownOrganism', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render alerts and dropdowns when data is provided', () => {
+    component.alertsGroupData = MOCK_ALERT_DROPDOWN_CONFIG.alertsGroupData;
+    component.dropdownGroupData = MOCK_ALERT_DROPDOWN_CONFIG.dropdownGroupData;
+    fixture.detectChanges();
+
+    const section = fixture.debugElement.query(By.css('section'));
+    expect(section).toBeTruthy();
+
+    const alertGroup = section.query(By.css('dsb-alert-group'));
+    expect(alertGroup).toBeTruthy();
+
+    const dropdownGroup = section.query(By.css('dsb-dropdown-group-molecule'));
+    expect(dropdownGroup).toBeTruthy();
+  });
+
+  it('should not render section when there is no data', () => {
+    component.alertsGroupData = [];
+    component.dropdownGroupData = [];
+    fixture.detectChanges();
+
+    const section = fixture.debugElement.query(By.css('section'));
+    expect(section).toBeNull();
   });
 });
